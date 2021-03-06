e79ddf6f22dbab657cc7edbdab81ed445bfb3294
Dalton Walker <dalton_walker@icloud.com>
2021-03-06 01:32:10 -0500

    Added parallel loop, no speed change

--- genmat/code/__main__.py
+++ genmat/code/__main__.py
@@ -3,9 +3,40 @@ import pandas as pd
 import math as math
 import os
 import joblib
+import time
 from drain3.drain import Drain
 from datetime import timedelta
 from genmat.database_methods import create_connection
+from multiprocessing import Process, Manager
+
+
+def genmat(cd, idx, di, labels, considered_container_names, tf, dd):
+    ret = {label: {} for label in labels}
+    for label in labels:
+        dl = di[di['label'] == label]
+        start_time = dl['timestamp'].iloc[0]
+        end_time = dl['timestamp'].iloc[-1]
+        n_intervals = math.ceil((end_time - start_time) / tf)
+        for container_name in considered_container_names:
+            logs = di[(di['label'] == label) & (di['container_name'] == container_name)]
+            drain_model = dd[container_name]
+            counter_array = np.zeros((len(drain_model.clusters), n_intervals))
+            for log in logs.itertuples():
+                time_stamp = log.timestamp
+                # instead of iterating over each time period we can calculate which period the log arrived
+                col = math.floor((time_stamp - start_time) / tf)
+                match_cluster, update_type = drain_model.add_log_message(log.log)
+                counter_array[match_cluster.cluster_id - 1, col] += 1
+            ret[label][container_name] = counter_array
+
+    cd[idx] = ret
+
+
+def generate_drain_models(dd, df, container):
+    drain_model = Drain()
+    for log in df.itertuples():
+        drain_model.add_log_message(log.log)
+    dd[container] = drain_model
 
 
 def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
@@ -47,11 +78,19 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
     # the goal here is to make a drain model for each container that has clusters for all observations
     # from all collections so that we may see which receive messages during specific collection runs
 
-    dd = {container_name: Drain() for container_name in considered_container_names}
-
-    for log in df.itertuples():
-        if log.container_name not in forbidden_containers:
-            dd[log.container_name].add_log_message(log.log)
+    print("Building drain models...")
+    ldict = {container: df[df['container_name'] == container] for container in considered_container_names}
+    # dd = {container_name: Drain() for container_name in considered_container_names}
+    drain_manager = Manager()
+    dd = drain_manager.dict()
+    drain_job = [Process(target=generate_drain_models,
+                         args=(dd, ldict[container], container))
+                 for container in considered_container_names]
+    _ = [process.start() for process in drain_job]
+    _ = [process.join() for process in drain_job]
+    # for log in df.itertuples():
+    #    if log.container_name not in forbidden_containers:
+    #        dd[log.container_name].add_log_message(log.log)
 
     # now that we have these drain models we can analyze each collection:
 
@@ -59,27 +98,17 @@ def cluster_visualizer(df: pd.DataFrame, tf: timedelta):
     dfs = {idx: df.iloc[ce[idx] + 1:ce[idx + 1] + 1, :].sort_values(by='timestamp') for idx in range(n_collections - 2)}
 
     # build the cluster count dictionary
-    cd = {idx: {label: {container_name: np.array for container_name in considered_container_names} for label in labels}
-          for idx in dfs.keys()}
 
-    for idx in dfs.keys():
-        di = dfs[idx]
-        for label in labels:
-            dl = di[di['label'] == label]
-            start_time = dl['timestamp'].iloc[0]
-            end_time = dl['timestamp'].iloc[-1]
-            n_intervals = math.ceil((end_time - start_time) / tf)
-            for container_name in considered_container_names:
-                logs = di[(di['label'] == label) & (di['container_name'] == container_name)]
-                drain_model = dd[container_name]
-                counter_array = np.zeros((len(drain_model.clusters), n_intervals))
-                for log in logs.itertuples():
-                    time_stamp = log.timestamp
-                    # instead of iterating over each time period we can calculate which period the log arrived
-                    col = math.floor((time_stamp - start_time) / tf)
-                    match_cluster, update_type = drain_model.add_log_message(log.log)
-                    counter_array[match_cluster.cluster_id - 1, col] += 1
-                cd[idx][label][container_name] = counter_array
+    # for idx in dfs.keys():
+
+    manager = Manager()
+    cd = manager.dict()
+    # cd = {idx: {label: {container_name: np.array for container_name in considered_container_names} for label in labels}
+    #      for idx in dfs.keys()}
+    print("beginning parallel section")
+    job = [Process(target=genmat, args=(cd, i, dfs[i], labels, considered_container_names, tf, dd)) for i in dfs.keys()]
+    _ = [process.start() for process in job]
+    _ = [process.join() for process in job]
 
     return labels, considered_container_names, dfs.keys(), cd, dd
 
@@ -101,9 +130,13 @@ def main():
     joblib.dump(labels, "/code/results/labels.joblib")
     joblib.dump(containers, "/code/results/containers.joblib")
     joblib.dump(set(collections), "/code/results/collections.joblib")
-    joblib.dump(cd, "/code/results/matrices_dict.joblib")
-    joblib.dump(dd, "/code/results/drain_dict.joblib")
+    joblib.dump(dict(cd), "/code/results/matrices_dict.joblib")
+    joblib.dump(dict(dd), "/code/results/drain_dict.joblib")
     print("... saving complete!")
 
+
 if __name__ == '__main__':
+    start = time.time()
     main()
+    print(f"Runtime: {time.time()-start}")
+
